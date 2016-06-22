a8d72ec33665c786d5728b78f5ec60ba3c339433
Frank Tang <roadtang@gmail.com>
2016-06-22 20:34:45 +0800

    admin - polls - fields & display

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+    list_display = ('question_text', 'pub_date')
+
+
+admin.site.register(Question, QuestionAdmin)
