81f586540183a643721e70da8cd0f205277ef6a1
Frank Tang <roadtang@gmail.com>
2016-06-22 17:52:44 +0800

    polls - test - detail view

--- polls/tests.py
+++ polls/tests.py
@@ -53,8 +53,14 @@ class QuestionViewTests(TestCase):
     def test_index_view_with_no_question(self):
         resp  = self.client.get(reverse('polls:index'))
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp, "No polls are available")
-        self.assertQuerysetEqual(resp.context(['latest_question_list'], []))
+        self.assertContains(resp, "No polls are available")
+        self.assertQuerysetEqual(resp.context['latest_question_list'], [])
+
+    def test_index_view_with_a_future_question(self):
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_index_view_with_a_past_question(self):
         create_question(question_text="Past question.", days=-30)
@@ -63,12 +69,6 @@ class QuestionViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question.>']
         )
-    
-    def test_index_view_with_a_future_question(self):
-        create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse('polls:index'))
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_index_view_with_future_question_and_past_question(self):
         """
@@ -98,6 +98,27 @@ class QuestionViewTests(TestCase):
 
         
 
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        '''
+        should return 404
+        '''
+        q = create_question(question_text='Future question', days=5)
+        url = reverse('polls:detail', args(q.id,))
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 404)
 
+
+    def test_detail_view_with_a_past_question(self):
+        '''
+        should display question's text
+        '''
+        q = create_question(question_text='Past Question', days=-5)
+        url = reverse('polls:detail', args=(q.id,))
+        resp = self.client.get(url)
+        self.assertContains(resp, q.question_text)
+
+
+    
         
 
