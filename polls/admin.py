07b55652d4e8edaeced5009c7c4393d20ea4bfd4
Frank Tang <roadtang@gmail.com>
2016-06-22 22:26:57 +0800

    poll - admin - question.was_published_recently

--- polls/admin.py
+++ polls/admin.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     #fields = ['pub_date', 'question_text']
 
@@ -11,7 +15,9 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
